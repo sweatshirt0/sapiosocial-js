581889d9326f72ef6cb9fbbb75760ec90ec2deb6
sweatshirt0 <sweatshirttt@proton.me>
2023-01-12 14:36:53 -0500

    footer addition

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+	return (
+		<div class="footer-wrapper">
+			<h4 class="IRC-header">IRC</h4>
+			<p class="IRC-contacts">Liberta server: sweatshirt, samsepi0l<br/>
+			Libera server: sweatshirt</p>
+		</div>
+	);
+}
+
+export default Footer
