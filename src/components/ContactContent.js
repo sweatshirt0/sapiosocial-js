49cea3ad46f46df4c120e9fcbe374ba03f2a2954
sweatshirt0 <sweatshirttt@proton.me>
2023-01-16 11:01:01 -0500

    contact info added

--- src/components/ContactContent.js
+++ src/components/ContactContent.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const ContactContent = () => {
 	return (
-		<div class="contact-content">
+		<div class="contact-content-wrapper">
 			<h3 class="contact-name">William "Christopher" West</h3>
+			<p class="contact-content">Email: chris@kloutmachine.com<br/>
+			IRC username: sweatshirt</p>
 		</div>
 	);
 }
